92117c2ac304601debea0028d3bb8cff8c681a67
njoskuykuy <njoskuykuy@gmail.com>
2018-10-13 13:56:06 +0700

    Edit About

--- pages/about.php
+++ pages/about.php
@@ -1,1 +1,9 @@
-<h1>ini About bray</h1>
+<div class="jumbotron">
+	<div class="container">
+		<h1>Ini About Me</h1>
+		<p>Saya kece baday</p>
+		<p>
+			<a class="btn btn-primary btn-lg">Learn more</a>
+		</p>
+	</div>
+</div>
