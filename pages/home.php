d7f0671a2ab241d793e37b89cab3797576ffdd78
njoskuykuy <njoskuykuy@gmail.com>
2018-10-13 13:54:00 +0700

    Perubahan home

--- pages/home.php
+++ pages/home.php
@@ -1,9 +1,11 @@
 <div class="jumbotron">
 	<div class="container">
-		<h1>Hello, world!</h1>
-		<p>Contents ...</p>
+		<h1>Ini Welcome to my Site.</h1>
+		<p>Follow my Social Media</p>
 		<p>
-			<a class="btn btn-primary btn-lg">Learn more</a>
+			<a class="btn btn-primary btn-lg">Instagram</a>
+			<a class="btn btn-primary btn-lg">Facebook</a>
+			<a class="btn btn-primary btn-lg">Github</a>
 		</p>
 	</div>
 </div>
